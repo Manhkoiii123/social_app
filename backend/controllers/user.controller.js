0f45b377c36c938f02ee18966ec57f6f107e9d7f
manhtranduc <manhtranduc0202@gmail.com>
2025-08-02 21:16:51 +0700

    upload image and api profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 export const getCurrentUser = async (req, res) => {
   try {
@@ -23,6 +24,50 @@ export const suggestedUser = async (req, res) => {
       .limit(5)
       .select("-password");
     res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const editProfile = async (req, res) => {
+  try {
+    const { name, userName, bio, profession, gender } = req.body;
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const sameUserWithUserName = await User.findOne({
+      userName,
+      _id: { $ne: req.userId },
+    });
+    if (sameUserWithUserName) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    let profileImage;
+    if (req.file) {
+      profileImage = await uploadOnCloudinary(req.file.path);
+    }
+    user.name = name;
+    user.userName = userName;
+    user.bio = bio;
+    user.profession = profession;
+    user.gender = gender;
+    user.profileImage = profileImage;
+    await user.save();
+    return res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getProfile = async (req, res) => {
+  try {
+    const userName = req.params.userName;
+    const user = await User.findOne({ userName }).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
